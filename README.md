6c5d9bf70d0abd531039672662fd531fe140c51a
shapoco <40152459+shapoco@users.noreply.github.com>
2025-06-27 11:39:44 +0900

    add: sandpaper-attachment

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # jigs
-治具などの雑多な3Dモデル
+
+治具などの雑多な3Dモデル。
+
+----
+
+## CadQuery (CQ-Editor) の使用方法
+
+### 初期設定 (Ubuntu24.04.1 LTS / WSL2)
+
+CQ-Editor を使用する前に毎回以下を実行してください。
+
+```bash
+source ./venv-setup.shrc
+```
+
+初回は venv (仮想環境) に CQ-Editor がインストールされます。
+
+また、`CQ-editor` が `$(pwd)/venv/bin/CQ-editor` のエイリアスになります。
+エイリアスは端末を閉じるまで有効です。
+
+### CQ-Editor によるファイルの開き方
+
+初期設定を実施した後、以下を実行します。
+`path/to/script.cq.py` はスクリプトファイルのパスです。
+
+```bash
+CQ-editor path/to/script.cq.py &
+```
+
+モデルがレンダリングされない場合は ▶ ボタンをクリックしてみてください。
+
+以降はファイルが更新されるたびにスクリプト内の `show_object()` の呼び出しに従って再描画されます。
+
+> [!NOTE]
+> 以下のようなエラーが表示される場合、パッケージが不足している可能性があります。
+>
+> ```
+> qt.qpa.plugin: Could not load the Qt platform plugin "xcb" in "" even though it was found.
+> This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem.
+> ```
+>
+> Qt のことはよく分からないですが、「[PyQt5をUbuntuで使うときにGUI周りでエラー (WSL2)](https://qiita.com/momomo_rimoto/items/83917d3f9f5dd35457e1)」を参考に以下のパッケージを入れたところ解決しました。
+>
+> ```bash
+> sudo apt install \
+>   libxkbcommon-x11-0 \
+>   libxcb-icccm4 \
+>   libxcb-image0 \
+>   libxcb-keysyms1 \
+>   libxcb-render-util0 \
+>   libxcb-xinerama0
+> ```
+
---
+++ tool/sandpaper-attachment/r1/sandpaper-attachment.cq.py
@@ -0,0 +1,136 @@
+import cadquery as cq
+import math
+import os
+
+# 円盤の直径
+DISC_DIAMETER = 20
+
+# 球面の半径
+SPHERE_RADIUS = 20
+
+# シャフトの直径
+SHAFT_DIAMETER = 2
+
+# シャフトの穴の直径マージン
+SHAFT_HOLE_DIAMETER_MARGIN = 0.5
+
+# シャフト取り付け穴の深さ
+SHAFT_HOLE_DEPTH = 15
+
+# シャフトガイドの壁厚
+SHAFT_GUIDE_THICKNESS = 2
+
+# 曲面のパラメータ
+curve_angle = math.asin((DISC_DIAMETER / 2) / SPHERE_RADIUS)
+curve_height = (1 - math.cos(curve_angle)) * SPHERE_RADIUS
+
+# ディスクの最小厚さ
+disc_min_thickness = 3
+
+# キャップの最小厚さ
+cap_min_thickness = 3
+
+# シャフトガイドの直径
+shaft_guide_diameter = SHAFT_DIAMETER + SHAFT_GUIDE_THICKNESS * 2
+
+# シャフトガイドの長さ (穴を円盤の内部までめり込ますためその分短くする)
+shaft_guide_length = SHAFT_HOLE_DEPTH - curve_height - disc_min_thickness / 2
+
+# 円柱
+cylinder_height = cap_min_thickness + curve_height + disc_min_thickness
+cylinder = (
+    cq.Workplane("XY")
+    .cylinder(cylinder_height, DISC_DIAMETER / 2, centered=(True, True, False))
+)
+
+# 球面
+sphere = (
+    cq.Workplane("XY")
+    .sphere(SPHERE_RADIUS)
+    .translate((0, 0, SPHERE_RADIUS + cap_min_thickness))
+)
+
+# アタッチメント
+attachment = (
+    cylinder
+    
+    # 球面の形成
+    .intersect(sphere)
+    
+    # シャフトガイドの押し出し
+    .faces(">Z")
+    .workplane()
+    .pushPoints([(0, 0)])
+    .circle(shaft_guide_diameter / 2)
+    .extrude(shaft_guide_length)
+    
+    # シャフト用の穴
+    .faces(">Z")
+    .workplane()
+    .pushPoints([(0, 0)])
+    .circle((SHAFT_DIAMETER + SHAFT_HOLE_DIAMETER_MARGIN) / 2)
+    .cutBlind(-SHAFT_HOLE_DEPTH)
+    
+    # 面取り
+    .edges("%circle")
+    .edges(">Z")[1]
+    .chamfer(0.5)
+    
+    # 接地
+    .translate((0, 0, -cap_min_thickness))
+)
+
+# シャフトガイドの根元の面取り
+shaft_guide_chamfer = min(2, (DISC_DIAMETER - shaft_guide_diameter) / 2)
+if shaft_guide_chamfer > 0:
+    attachment = (
+        attachment
+        .edges("%circle")
+        .edges(">>Z[-3]")[0]
+        .chamfer(2)
+    )
+
+# やすり固定用のキャップ
+cap = cylinder.cut(sphere)
+
+# やすり固定用のパイプ
+pipe_wall_thickness = 5
+pipe_length = shaft_guide_length + 1
+pipe_inner_diameter = max(DISC_DIAMETER - (pipe_wall_thickness * 2), shaft_guide_diameter + 1)
+pipe_outer_diameter = pipe_inner_diameter + (pipe_wall_thickness * 2)
+pipe = (
+    # ボディ
+    cq.Workplane("XY")
+    .cylinder(pipe_length, pipe_outer_diameter / 2, centered=(True, True, False))
+    
+    # 穴
+    .faces(">Z")
+    .workplane()
+    .pushPoints([(0, 0)])
+    .circle(pipe_inner_diameter / 2)
+    .cutBlind(-pipe_length - 1)
+)
+
+# シャフトガイドの根元の面取りに対応したパイプ内側の面取り
+pipe_chamfer = min(pipe_wall_thickness / 2, 1 + (shaft_guide_diameter / 2 + shaft_guide_chamfer) - (pipe_inner_diameter / 2))
+if pipe_chamfer > 0:
+    pipe = (
+        pipe
+        .edges("%circle")
+        .edges("<Z")[1]
+        .chamfer(pipe_chamfer)
+    )
+
+# 表示
+display_offset = DISC_DIAMETER + 10
+show_object(attachment, name="Attachment")
+show_object(cap.translate((display_offset, 0, 0)), name="Cap")
+show_object(pipe.translate((-display_offset, 0, 0)), name="Pipe")
+
+# 出力
+for fmt in ["step", "stl"]:
+    dir_name = f"d{DISC_DIAMETER}_r{SPHERE_RADIUS}_s{SHAFT_DIAMETER}/{fmt}"
+    os.makedirs(dir_name, exist_ok=True)
+    attachment.export(f"{dir_name}/attachment.{fmt}")
+    cap.export(f"{dir_name}/cap.{fmt}")
+    pipe.export(f"{dir_name}/pipe.{fmt}")
